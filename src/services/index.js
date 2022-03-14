cd128089a715fb1c2266c940ce83c667dd001ceb
Hernan Diaz <hldiaz@teco.com.ar>
2022-03-14 21:48:44 +0100

    refactor

--- src/services/index.js
+++ src/services/index.js
@@ -4,10 +4,10 @@ const payload = ([launchesInfo, rocketsInfo]) => {
 
     return launchesInfo.map( ({flight_number, mission_name, rocket}) => {
         
-        const { description, flickr_images} = rocketsInfo.find( rocket => rocket.rocket_id === rocket.rocket_id);
         const {rocket_id, rocket_name} = rocket;
+        const { description, flickr_images: images} = rocketsInfo.find( rocketElement => rocketElement.rocket_id === rocket_id);
 
-        const {payload_id, manufacturer, payload_type} = rocket.second_stage?.payloads[0];
+        const {payload_id, manufacturer, payload_type: type} = rocket.second_stage?.payloads[0];
 
         return {
             flight_number, 
@@ -16,12 +16,12 @@ const payload = ([launchesInfo, rocketsInfo]) => {
                 rocket_id,
                 rocket_name,
                 description,
-                images: flickr_images
+                images
             },
             payloads: [{
                 payload_id,
                 manufacturer,
-                type: payload_type
+                type
             }]
     }});
 }
